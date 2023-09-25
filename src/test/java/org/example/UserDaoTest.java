378e6652ed6cfff6af6cb6a3a1266c7a65f1673e
shinsang97 <103565931+shinsang97@users.noreply.github.com>
2023-09-25 14:56:03 +0900

    h2를 사용해 조회 완료

--- src/test/java/org/example/UserDaoTest.java
+++ src/test/java/org/example/UserDaoTest.java
@@ -1,9 +1,14 @@
 package org.example;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserDaoTest {
     @BeforeEach
@@ -13,5 +18,13 @@ public class UserDaoTest {
         DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
     }
 
+@Test
+    void createTest() throws SQLException {
+        UserDao userDao = new UserDao();
 
+        userDao.create(new User("wizard", "password", "name", "email"));
+
+        User user = userDao.findByUserId("wizard");
+        assertThat(user).isEqualTo(new User("wizard", "password", "name", "email"));
+}
 }
